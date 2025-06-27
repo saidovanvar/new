48d5d806bf09a5b10f5e402160396bebbb5b5772
saidovanvar <saidovanvar@gmail.com>
2025-06-27 14:51:56 +0500

    new

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 let form = document.querySelector('#form');
 let name = document.querySelector('#name');
 let exams = document.querySelector(".exams");
-
-
+let editId
+let add = document.querySelector('.add');
+let a = false
 const endpoints = {
     section: "Sections",
     category: "Categories",
     lead: "Leads"
 };
+if(a){
+    add.textContent = "updata"
+}else{
+    add.textContent = "add"
+}
 
 let dataFetch = async () => {
     let response = await fetch(`https://crm-test-api.duckdns.org/api/Sections`, {
@@ -30,8 +36,14 @@ function fetchSections(sections) {
         li.innerHTML = `
       <div class="border bg-red-300 px-[20px] py-[20px]">
         <p class="text-black">${section.name}</p>
-        <button data-id="${section.id}" data-type="section" class="btn_delete bg-red-500 p-4 w-full text-white mt-4 rounded-md hover:bg-blue-400">Delete Section</button>
+        <div class="flex gap-4">
+           <button data-id="${section.id}" data-type="section" class="btn_delete bg-red-500 p-4 w-full text-white mt-4 rounded-md hover:bg-blue-400">Delete Section</button>
+              <button data-id="${section.id}" class="btn_edit bg-yellow-500 p-4 w-[150px] text-white cursor-pointer duration-150 mt-4 rounded-md hover:bg-yellow-700">
+         Edit </button>
+</div>
+     
       </div>
+
     `;
 
         section.categories.forEach(category => {
@@ -40,7 +52,10 @@ function fetchSections(sections) {
         <div class="border bg-red-300 px-[20px] py-[20px]">
           <p class="text-black">${category.name}</p> 
           <button data-id="${category.id}" data-type="category" class="btn_delete bg-red-500 p-4 w-full text-white mt-4 rounded-md hover:bg-blue-400">Delete Category</button>
+           <button data-id="${category.id}" class="btn_edit bg-yellow-500 p-4 w-full text-white cursor-pointer duration-150 mt-4 rounded-md hover:bg-yellow-700">
+         Edit </button>
         </div>
+       
       `;
 
             category.leads.forEach(lead => {
@@ -52,7 +67,10 @@ function fetchSections(sections) {
             <p>${lead.phoneNumber}</p> 
             <p>${lead.comment}</p>
             <button data-id="${lead.id}" data-type="lead" class="btn_delete bg-red-500 p-4 w-full text-white mt-4 rounded-md hover:bg-blue-400">Delete Lead</button>
+                 <button data-id="${lead.id}" class="btn_edit bg-yellow-500 p-4 w-full text-white cursor-pointer duration-150 mt-4 rounded-md hover:bg-yellow-700">
+         Edit </button>
           </div>
+       
         `;
                 exams.appendChild(leadText);
             });
@@ -63,7 +81,7 @@ function fetchSections(sections) {
         exams.appendChild(li);
     });
 
-
+    let btn_edit = document.querySelectorAll(".btn_edit");
     let btn_delete = document.querySelectorAll(".btn_delete");
     btn_delete.forEach(button => {
         button.addEventListener('click', async (e) => {
@@ -72,6 +90,28 @@ function fetchSections(sections) {
             await deleteByType(id, type);
         });
     });
+    btn_edit.forEach(edit => {
+        edit.addEventListener('click', async (e) => {
+            let id = e.target.getAttribute('data-id');
+            await editAction(id)
+
+
+        });
+    });
+}
+async function editAction(id) {
+    let response = await fetch(`https://crm-test-api.duckdns.org/api/Sections/${id}/` , {
+        method: 'GET',
+        headers: {
+            "authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwibmFtZSI6ImFzaWxiZWsiLCJ1bmlxdWVfbmFtZSI6Iis5OTg5MTQ1NDIzMzkiLCJqdGkiOiIzMjJjM2Y4Yi1mMTMxLTRkNmUtYmE2Zi1iZTlkMTY4MDQ2MmUiLCJUZW5hbnRJZCI6IjEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsImV4cCI6MTc1NjkxNTc5NCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIn0.EYljRkOlh94fuprfOFInoSv51vhKtviP1icgZqik7Dw`
+        }
+    })
+    let data = await response.json();
+    name.value = data.name;
+    editId = id
+    a = true
+
+
 }
 
 
@@ -96,33 +136,57 @@ async function deleteByType(id, type) {
         alert(`${type} o'chirildi`);
         dataFetch();
     } else {
-        alert(`O‘chirishda xatolik: ${res.status}`);
+        alert(` xatolik:`);
     }
 }
 
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
+    let response = async ()=> {
+        let post
 
-    let response = await fetch(`https://crm-test-api.duckdns.org/api/Sections`, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-            "authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwibmFtZSI6ImFzaWxiZWsiLCJ1bmlxdWVfbmFtZSI6Iis5OTg5MTQ1NDIzMzkiLCJqdGkiOiIzMjJjM2Y4Yi1mMTMxLTRkNmUtYmE2Zi1iZTlkMTY4MDQ2MmUiLCJUZW5hbnRJZCI6IjEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsImV4cCI6MTc1NjkxNTc5NCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIn0.EYljRkOlh94fuprfOFInoSv51vhKtviP1icgZqik7Dw`
-        },
-        body: JSON.stringify({
-            name: name.value
-        })
-    });
+        if (a){
 
-    await response.json();
 
-    if (response.ok) {
-        alert("Yangi bo‘lim qo‘shildi");
-        dataFetch();
-    } else {
-        alert("Qo‘shishda xatolik");
+            post = await fetch(`https://crm-test-api.duckdns.org/api/Sections/${editId}/`, {
+
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwibmFtZSI6ImFzaWxiZWsiLCJ1bmlxdWVfbmFtZSI6Iis5OTg5MTQ1NDIzMzkiLCJqdGkiOiIzMjJjM2Y4Yi1mMTMxLTRkNmUtYmE2Zi1iZTlkMTY4MDQ2MmUiLCJUZW5hbnRJZCI6IjEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsImV4cCI6MTc1NjkxNTc5NCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIn0.EYljRkOlh94fuprfOFInoSv51vhKtviP1icgZqik7Dw`
+                },
+                body: JSON.stringify({
+                    name: name.value
+                })
+            });
+
+        } else {
+
+
+            post = await fetch(`https://crm-test-api.duckdns.org/api/Sections`, {
+
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwibmFtZSI6ImFzaWxiZWsiLCJ1bmlxdWVfbmFtZSI6Iis5OTg5MTQ1NDIzMzkiLCJqdGkiOiIzMjJjM2Y4Yi1mMTMxLTRkNmUtYmE2Zi1iZTlkMTY4MDQ2MmUiLCJUZW5hbnRJZCI6IjEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsImV4cCI6MTc1NjkxNTc5NCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIn0.EYljRkOlh94fuprfOFInoSv51vhKtviP1icgZqik7Dw`
+                },
+                body: JSON.stringify({
+                    name: name.value
+                })
+            })
+
+        }
+
+
+        await response.json();
+
+        if (response.ok) {
+            alert(" qo‘shildi");
+            dataFetch();
+        } else {
+            alert(" xatolik");
+        }
     }
-});
-
-dataFetch();
+    dataFetch();
+})
